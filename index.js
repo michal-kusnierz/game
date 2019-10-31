47fbf81d2e7a233964c58a1221150009035e70bd
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-31 15:01:21 +0100

    RenderObstacles and RenderSkyItems

--- index.js
+++ index.js
@@ -78,3 +78,128 @@ class Move {
 
 Move.RenderPlane(PlaneJS);
 document.addEventListener("keydown", event => Move.Collision(PlaneJS, event));
+
+
+/////////moving obstacles//////////
+const 
+  startBtn = document.getElementById('start__btn'),
+  startGame = () => {
+    startBtn.style.display = 'none';
+    event.preventDefault();
+    setInterval(() => RenderObstacles.RenderObst(),300);
+    setInterval(() => RenderSkyItems.RenderSky(),300);
+    setInterval(() => RenderSkyImg.RenderSkyI(),310);
+    document.addEventListener("keydown", event => Move.KeySupport(PlaneJS, event));
+};
+
+
+class Obstacles {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+  get x() {
+    return this._x;
+  }
+  get y() {
+    return this._y;
+  }
+
+  left() {
+    this._x -= 10;
+  }
+}
+const ObstJS = new Obstacles(-80, 300);
+const ObstJSCity1 = new Obstacles(370, 390);
+const ObstJSCity2 = new Obstacles(640, 390);
+const ObstJSCity3 = new Obstacles(760, 390);
+
+class RenderObstacles {
+  static RenderObst() {
+    const ObstMount = document.querySelector('.mount');
+    const ObstCity1 = document.querySelector('.city1');
+    const ObstCity2 = document.querySelector('.city2');
+    const ObstCity3 = document.querySelector('.city3');
+    ObstMount.style.left = ObstJS.x + "px";
+    ObstCity1.style.left = ObstJSCity1.x + "px";
+    ObstCity2.style.left = ObstJSCity2.x + "px";
+    ObstCity3.style.left = ObstJSCity3.x + "px";
+    ObstJS.left();
+    ObstJSCity1.left();
+    ObstJSCity2.left();
+    ObstJSCity3.left();
+    }
+  }
+
+////////////moving sky-items///////
+  class SkyItems {
+    constructor(x, y) {
+      this._x = x;
+      this._y = y;
+    }
+    get x() {
+      return this._x;
+    }
+    get y() {
+      return this._y;
+    }
+  
+    left() {
+      this._x -= 15;
+    }
+  }
+  const SkyJSBird1 = new SkyItems(340, 60);
+  const SkyJSBird2 = new SkyItems(840, 30);
+  const SkyJSCloud1 = new SkyItems(80, 20);
+  const SkyJSCloud2 = new SkyItems(150, 80);
+  const SkyJSCloud3 = new SkyItems(540, 30);
+  
+  class RenderSkyItems {
+    static RenderSky() {
+      const SkyBird1 = document.querySelector('.bird1');
+      const SkyBird2 = document.querySelector('.bird2');
+      const SkyCloud1 = document.querySelector('.cloud1');
+      const SkyCloud2 = document.querySelector('.cloud2');
+      const SkyCloud3 = document.querySelector('.cloud3');
+      SkyBird1.style.left = SkyJSBird1.x + "px";
+      SkyBird2.style.left = SkyJSBird2.x + "px";
+      SkyCloud1.style.left = SkyJSCloud1.x + "px";
+      SkyCloud2.style.left = SkyJSCloud2.x + "px";
+      SkyCloud3.style.left = SkyJSCloud3.x + "px";
+      SkyJSImg.left();
+      SkyJSBird1.left();
+      SkyJSBird2.left();
+      SkyJSCloud1.left();
+      SkyJSCloud2.left();
+      SkyJSCloud3.left();
+      }
+    }
+
+    class SkyImg {
+      constructor(x, y) {
+        this._x = x;
+        this._y = y;
+      }
+      get x() {
+        return this._x;
+      }
+      get y() {
+        return this._y;
+      }
+    
+      left() {
+        this._x -= 10;
+      }
+    }
+    const SkyJSImg = new SkyImg(580, 130);
+    
+    class RenderSkyImg {
+      static RenderSkyI() {
+        const SkyImg = document.querySelector('img');
+        SkyImg.style.left = SkyJSImg.x + "px";
+        SkyJSImg.left();
+      }
+    }
+  
+
+startBtn.addEventListener('click', startGame);
