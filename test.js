99f5400615e0c22cd727bd052b70a0dd1bd04808
Michal Kusnierz <55190043+michal-kusnierz@users.noreply.github.com>
2019-11-14 17:54:13 +0100

    take2 - collision of two objects in arrays

--- test.js
+++ test.js
@@ -22,3 +22,27 @@ gameOver = () => {console.log('this is the end')}
 check()
 
 // version with nested position.x and position.y
+
+const arrObj1 = [
+  {id: 0, position: {xA:100, yA:100}},
+];
+
+const arrObj2 = [
+  {id:1, position: {xB:1, yB: 20}},
+  {id:2, position: {xB:20, yB: 20}},
+  {id:3, position: {xB:100, yB: 100}},
+  {id:4, position: {xB:50, yB: 50}},
+];
+
+const take2 = arrObj2.filter(
+  item => item.position.xB === arrObj1[0].position.xA && item.position.yB === arrObj1[0].position.yA);
+// console.log(`check id is: ` + checkArr[0].id)
+
+checkObj = () => {
+  take2 ? gameOver2() : console.log('nothing')
+}
+
+gameOver2 = () => {console.log('collision of two objects')}
+
+checkObj()
+
